4d976d03556cb9b1b991c6755d91b622b3363b13
jangmi <jsy639@naver.com>
2024-12-26 15:07:53 +0900

    아이디 중복 체크 기능 추가

--- app/src/main/java/mincheol/memberapplication/helper/DatabaseHelper.java
+++ app/src/main/java/mincheol/memberapplication/helper/DatabaseHelper.java
@@ -2,6 +2,7 @@ package mincheol.memberapplication.helper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -11,6 +12,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     //데이터베이스 초기화 변수
+    //device explorer >> data >> 앱 패키지 >>databases
     private static final String DBNAME = "android.db";
     private static final int DBVERSION = 1;
 
@@ -61,4 +63,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     //아이디 중복 확인
+    public boolean useridCheck(String userid){
+        //아이디 중복 체크를 위해 sqlite 초기화
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        //커서 초기화
+        //select mno from member where userid=?
+        //query(table, columns, select,groupby,having,orderby)
+        Cursor cur = db.query("member",new String[]{"mno"},"userid=?",new String[]{userid},null,null,null);
+
+        //조회 결과 확인
+        boolean exists= cur.getCount()>0;
+
+        //디비 연결 해제
+        cur.close();
+        db.close();
+
+        return exists;
+    }
 }
