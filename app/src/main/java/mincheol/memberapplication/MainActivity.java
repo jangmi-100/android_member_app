d6e73f68cf0247176afba13dcf0c5932b92161e2
jangmi <jsy639@naver.com>
2024-12-27 16:18:25 +0900

    인터넷 커미션 추가 MariaDB 라이브러리 추가 PackageOptions 설정 추가 비동기 처리를 위한 AsyncTask 클래스 추가

--- app/src/main/java/mincheol/memberapplication/MainActivity.java
+++ app/src/main/java/mincheol/memberapplication/MainActivity.java
@@ -2,6 +2,7 @@ package mincheol.memberapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -56,6 +57,11 @@ public class MainActivity extends AppCompatActivity {
         sharedPreferences = getSharedPreferences("UserSession",MODE_PRIVATE);
 
         //회원가입 이벤트 처리
+        // main Activity에서 네트워크 작업시 NetworkOnMainThreadException 발생!
+        // 즉, 메인쓰레드에서 네트워크 작업은 기본적으로 금지되어 있음
+        // 특정 액티비티가 네트워크를 독점적으로 점유하는 것을 방지
+        // 비동기 처리를 이용해서 코드를 작성해야 함
+        // >> AsyncTask, ExcutorService, Coroutines (코틀린 - 추천!)
         buttonJoin.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -132,4 +138,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    private class RegisterUserTask extends AsyncTask<String, Void,Boolean> {
+
+        //doInBackground
+        //시간이 오래 걸리는 작업 수행에 사용
+        //네트워크, 데이터베이스, 파일처리등의 작업 - UI 스레드가 차단
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String userid = params[0];
+            String passwd = params[1];
+            String name = params[2];
+            String email = params[3];
+
+            try{
+                //중복 아이디 체크
+                if (mariaDBHelper.useridCheck(userid)){
+                    return null;
+
+                }
+                //회원 정보 저장
+                return mariaDBHelper.insertMember(userid,passwd,name,email);
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+            return false;
+        }
+
+        //onPostExecute
+        //doInBackground의 결과를 수신해서 UI에 반영할때 사용
+        @Override
+        protected void onPostExecute(Boolean success){
+            if(success==null){//아이디가 중복이면
+                Toast.makeText(MainActivity.this,"😥이미 사용중인 아이디입니다!😥",Toast.LENGTH_SHORT).show();
+            }else if(success){//회원정보가 정상적으로 저장되었다면
+                Toast.makeText(MainActivity.this,"✨회원가입 성공!✨",Toast.LENGTH_SHORT).show();
+
+            }else{
+                Toast.makeText(MainActivity.this,"😱회원가입 실패! 다시 시도하세요!😱",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
+
